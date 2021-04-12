33824caf15668d50e7014da7e4409a7040dbde07
gdkim <oyammo@hanmail.net>
2021-04-12 16:31:22 +0900

    Initial commit

---
+++ src/main/java/com/raonse2/pms/PmsApplication.java
@@ -0,0 +1,13 @@
+package com.raonse2.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raonse2/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raonse2.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
