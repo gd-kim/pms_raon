057ddf678c459a2620cfe04bf123115e1030889c
sjseo <muiruat@naver.com>
2021-04-12 17:06:09 +0900

    sjseo commit

--- src/main/java/com/raonse2/pms/PmsApplication.java
+++ src/main/java/com/raonse2/pms/PmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PmsApplication {
 
     public static void main(String[] args) {
+        //2021-04-12 sjseo 추가
         SpringApplication.run(PmsApplication.class, args);
     }
 
