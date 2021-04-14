564a43d29bf75bf49b47b450afed442def5b77bb
gdkim <oyammo@hanmail.net>
2021-04-14 15:39:11 +0900

    controller 위치 변경

--- src/main/java/com/raonse2/pms/controller/MainController.java
+++ src/main/java/com/raonse2/pms/controller/MainController.java
@@ -10,6 +10,7 @@ public class MainController {
     @RequestMapping("/hello")
     public String hello(Model model) {
 
+
         model.addAttribute("name","김심평원프로님 무야호!");
         return "hello";
     }
