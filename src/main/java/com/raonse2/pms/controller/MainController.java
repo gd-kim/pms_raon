08d499b1d7ae7fe7fb12b1933b4c2d5dcfb90677
gdkim <oyammo@hanmail.net>
2021-04-14 15:38:36 +0900

    controller 위치 변경

--- src/main/java/com/raonse2/pms/controller/MainController.java
+++ src/main/java/com/raonse2/pms/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.raonse2.pms;
+package com.raonse2.pms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/test/java/com/raonse2/pms/MainControllerTest.java
+++ src/test/java/com/raonse2/pms/MainControllerTest.java
@@ -1,5 +1,6 @@
 package com.raonse2.pms;
 
+import com.raonse2.pms.controller.MainController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
